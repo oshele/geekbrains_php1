841482cf68c79b1d3ae2cd8138bc37dfbb983a70
vadosdog <vados.sparx@gmail.com>
2019-02-25 19:55:29 +0300

    preparation of lesson

---
+++ engine/db.php
@@ -0,0 +1,45 @@
+﻿<?php
+
+function showProduct($id)
+{
+	$result = getAssocResult("SELECT * FROM `products` WHERE `id` = $id");
+
+	return isset($result[0]) ? $result[0] : null;
+}
+
+function getProducts()
+{
+	return getAssocResult("SELECT * FROM `products`");
+}
+
+function createConnection()
+{
+	$db = mysqli_connect(HOST, USER, PASS, DB);
+	mysqli_query($db, "SET CHARACTER SET 'utf8'");
+	return $db;
+}
+
+function execQuery($sql)
+{
+	$db = createConnection();
+
+	$result = mysqli_query($db, $sql);
+
+	mysqli_close($db);
+	return $result;
+}
+
+function getAssocResult($sql)
+{
+	$db = createConnection();
+
+	$result = mysqli_query($db, $sql);
+
+	$array_result = [];
+	while ($row = mysqli_fetch_assoc($result)) {
+		$array_result[] = $row;
+	}
+	mysqli_close($db);
+	return $array_result;
+}
+
