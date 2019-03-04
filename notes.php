94d540125796e29cdaa8f423336e139f66b76fd2
vadosdog <vados.sparx@gmail.com>
2019-03-04 19:47:09 +0300

    lesson7 init

---
+++ notes.php
@@ -0,0 +1,53 @@
+﻿<?php
+
+setcookie("login", "lamer", time() + 3600 * 24 * 7);
+setcookie("password", "qwerty", time() + 3600 * 24 * 7);
+
+
+if (isset($_COOKIE['login'])) {
+	echo $_COOKIE['login'];
+}
+
+
+session_start();
+$_SESSION['name'] = 'name';
+$_SESSION['age'] = 13;
+
+
+unset($_SESSION['name']);
+unset($_SESSION['age']);
+
+session_destroy();
+
+
+/*
+<form method="post">
+        <p><input type="text" name="login" /></p>
+        <p><input type="password" name="password" /></p>
+        <p><input type="checkbox" name="rememberme" /></p>
+        <p><input type="submit" value="Войти" /></p>
+</form>
+*/
+
+
+/*
+
+CREATE TABLE `user` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`user_name` VARCHAR(50) NOT NULL,
+	`user_login` VARCHAR(50) NOT NULL,
+	`user_password` VARCHAR(60) NOT NULL,
+	`user_last_action` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`id_user`)
+)
+ENGINE=InnoDB;
+*/
+
+
+//location.reload()
+
+
+header('Content-Type: application/json');
+
+$json_string = json_encode($value);
+$array = json_decode($json_string, true);
---
+++ public/js/main.js
@@ -0,0 +1,37 @@
+﻿// $.post({
+// 	url: '/action.php?id=1',
+// 	data: {a: 1}
+// });
+
+
+function login() {
+	const $login_input = $('[name="login"]');
+	const $password_input = $('[name="password"]');
+
+	const login = $login_input.val();
+	const password = $password_input.val();
+
+	const $error_field = $('.errors');
+
+	console.log($error_field);
+
+	$.post({
+		url: '/api.php',
+		data: {
+			apiMethod: 'login',
+			postData: {
+				login: login,
+				password: password
+			}
+		},
+		success: function (data) {
+			data = JSON.parse(data);
+
+			if(data.error.length) {
+				$error_field.text(data.error);
+			} else {
+				location.reload();
+			}
+		}
+	});
+}
