c813ac20864d213bc67696be82ba824f494b4a2f
oshele <39349499+oshele@users.noreply.github.com>
2019-06-12 12:51:52 +0300

    Lesson7

--- public/products/index.php
+++ public/products/index.php
@@ -4,9 +4,11 @@ require_once __DIR__ . '/../../config/config.php';
 
 $catalogContent = renderProducts();
 
+
+
 echo render(TEMPLATES_DIR . 'index.tpl', [
 	'title' => 'Каталог',
 	'h1' => 'Каталог',
-	'content' => $catalogContent,
+	'content' => "<div class ='catalog'>$catalogContent</div>",
 	'style' => '../css/style.css'
 ]);
